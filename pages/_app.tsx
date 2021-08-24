4eccb2ad0e43b5e2e729890644c40a239af62d3f
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-08-23 23:19:51 -0300

    Add global context for iDB and SharedSheetInfo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import React, { FunctionComponent } from "react"
 import Head from "next/head"
+import dynamic from "next/dynamic"
 import { ThemeProvider } from "@material-ui/core/styles"
 import theme from "@/theme"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import { SharedSheetInfoProvider } from "@/shared-states/shared-sheet-info"
+
+const IndexedDBContextProvider = dynamic(
+  () => import("@/indexed-db/indexed-db-context"),
+  {
+    ssr: false,
+  }
+)
 
 export default function MyApp({
   Component,
@@ -14,16 +23,20 @@ export default function MyApp({
   return (
     <>
       <Head>
-        <title>Virtual Tabletop</title>
+        <title>Dice Overdrive</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <IndexedDBContextProvider>
+        <SharedSheetInfoProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </SharedSheetInfoProvider>
+      </IndexedDBContextProvider>
     </>
   )
 }
