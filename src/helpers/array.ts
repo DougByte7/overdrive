5cafdc790855ab63152b27e7cb6ffb6df9e0ed29
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-08-23 23:18:40 -0300

    Add isBlank helper

--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -2,5 +2,14 @@ export function newArray<T>(
   length: number,
   mapFn: (element: never, index: any) => T = (_, i) => i
 ): T[] {
-  return Array.from({ length }, mapFn);
+  return Array.from({ length }, mapFn)
+}
+
+//TODO create tests
+export function isBlank(value: any[] | Record<any, any>) {
+  return !!value
+    ? Array.isArray(value)
+      ? !(value as any[]).length
+      : typeof value === "object" && !Object.keys(value).length
+    : true
 }
