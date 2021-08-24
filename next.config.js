8c874255a7147152b33800f3395708beb6c9e372
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-08-23 23:16:00 -0300

    Add next i18n config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  i18n: {
+    locales: ["en-US", "pt-BR"],
+    defaultLocale: "en-US",
+    domains: [
+      {
+        domain: "diceoverdrive.com",
+        defaultLocale: "en-US",
+      },
+      {
+        domain: "br.diceoverdrive.com",
+        defaultLocale: "pt-BR",
+      },
+    ],
+  },
+}
