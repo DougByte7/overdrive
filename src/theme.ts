82868d82e5923db2ef9b9384c8d7409adf500a26
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-10-12 16:19:45 -0300

    Upadate material theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,9 @@
-import { createTheme } from "@material-ui/core/styles"
-import { red } from "@material-ui/core/colors"
-/* RGB 
-$white: rgba(251, 255, 254, 1);
-$dimGray: rgba(109, 103, 110, 1);
-$blackOlive: #3C393C;
-$brandPrimary: #FF6F00;
-$brandLight: #ff9100;
-$eerieBlack: rgba(27, 27, 30, 1);
-$smokyBlack: #0F0F11;
-$heidelbergRed: rgba(150, 3, 26, 1);
-// Create a theme instance.*/
+import { createTheme } from "@mui/material"
+import { red } from "@mui/material/colors"
+
 const theme = createTheme({
   palette: {
-    type: "dark",
+    mode: "dark",
     primary: {
       main: "#FF6F00",
     },
