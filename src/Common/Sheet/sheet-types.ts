2650556b78c4bf80f98ac21f11ef936827f6ea2a
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-10-12 16:21:35 -0300

    Add sheet input types

--- src/Common/Sheet/sheet-types.ts
+++ src/Common/Sheet/sheet-types.ts
@@ -7,21 +7,61 @@ export type GridPosition = {
   columnEnd: number
 }
 
-export type SheetInputField = {
-  type: SheetFieldType
+interface BaseSheetInputField {
   position: GridPosition
   label: string
-  value: string | number | string[]
-  options?: string[]
-  isMultiSelect?: boolean
 }
 
+export interface SheetInputTextField extends BaseSheetInputField {
+  type: "text"
+  value: string
+}
+
+export interface SheetInputNumberField extends BaseSheetInputField {
+  type: "number"
+  value: number
+}
+
+export interface SheetInputSelectField extends BaseSheetInputField {
+  type: "select"
+  value: string
+  options: string[]
+  isMultiSelect: boolean
+}
+
+export interface SheetInputCheckboxField extends BaseSheetInputField {
+  type: "checkbox"
+  value: number
+  quantity: number
+  isPrecisionRating: boolean
+  numberValue: number
+}
+
+export type SheetInputField =
+  | SheetInputTextField
+  | SheetInputNumberField
+  | SheetInputSelectField
+  | SheetInputCheckboxField
+
+export type SheetInputFieldKey =
+  | keyof SheetInputTextField
+  | keyof SheetInputNumberField
+  | keyof SheetInputSelectField
+  | keyof SheetInputCheckboxField
+
 export interface SheetDataBlock {
+  title?: string
   position: GridPosition
   inputFields: SheetInputField[]
 }
 
 export type ChangeSheetValuesEvent = (
   dataBlockIndex: number,
-  fieldIndex: number
-) => (event: React.ChangeEvent<HTMLInputElement>) => void
+  fieldIndex: number,
+  subValues?: SheetInputFieldKey
+) => (
+  event:
+    | React.ChangeEvent<HTMLInputElement>
+    | React.SyntheticEvent<Element, Event>,
+  value?: any
+) => void
