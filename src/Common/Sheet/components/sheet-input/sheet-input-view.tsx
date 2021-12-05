d8192870fe109135c9e9bb03f983cd7e160550fa
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-12-05 11:54:18 -0300

    Fix type inference

--- src/Common/Sheet/components/sheet-input/sheet-input-view.tsx
+++ src/Common/Sheet/components/sheet-input/sheet-input-view.tsx
@@ -1,140 +1,151 @@
 import {
-    TextFieldProps,
-    TextField,
-    Rating,
-    Box,
-    MenuItem,
-    Typography
+  TextFieldProps,
+  TextField,
+  Rating,
+  Box,
+  MenuItem,
+  Typography,
 } from "@mui/material"
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank"
+import CheckBoxIcon from "@mui/icons-material/CheckBox"
 import { isNil } from "lodash"
-import { GridPosition, SheetInputField, SheetInputFieldKey } from "@/common/sheet/sheet-types";
-import { ChangeEvent, FunctionComponent, useState } from "react";
+import type {
+  GridPosition,
+  SheetInputCheckboxField,
+  SheetInputField,
+  SheetInputFieldKey,
+  SheetInputSelectField,
+} from "@/common/sheet/sheet-types"
+import { ChangeEvent, FunctionComponent, useState } from "react"
 
 interface SheetInputProps {
-    input: SheetInputField,
-    inputIndex: number,
-    blockIndex: number
-    getGridArea: (position: GridPosition) => {
-        gridArea: string;
-    },
-    handleChangeSheetValues: (
-        dataBlockIndex: number,
-        fieldIndex: number,
-        value: number | string,
-        inputField?: SheetInputFieldKey
-    ) => void,
+  input: SheetInputField
+  inputIndex: number
+  blockIndex: number
+  getGridArea: (position: GridPosition) => {
+    gridArea: string
+  }
+  handleChangeSheetValues: (
+    dataBlockIndex: number,
+    fieldIndex: number,
+    value: number | string,
+    inputField?: SheetInputFieldKey
+  ) => void
 }
 export default function SheetInput(props: SheetInputProps) {
-    const {
-        input,
-        inputIndex,
-        blockIndex,
-        getGridArea,
-        handleChangeSheetValues
-    } = props
-    const { label, type, position, value: templateValue } = input
+  const {
+    input,
+    inputIndex,
+    blockIndex,
+    getGridArea,
+    handleChangeSheetValues,
+  } = props
+  const { label, type, position, value: templateValue } = input
 
+  const Input: FunctionComponent<TextFieldProps> = ({ children, ...rest }) => {
+    const [value, setValue] = useState(templateValue)
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.currentTarget.value)
+      handleChangeSheetValues(blockIndex, inputIndex, event.currentTarget.value)
+    }
+    const defaultProps: TextFieldProps = {
+      label,
+      value,
+      type,
+      style: getGridArea(position),
+      variant: "standard",
+      fullWidth: true,
+      onChange: handleChange,
+    }
+    return (
+      <TextField {...defaultProps} {...rest}>
+        {children}
+      </TextField>
+    )
+  }
 
+  switch (type) {
+    case "number":
+    case "text":
+      return <Input />
+    case "select": {
+      const { options, isMultiSelect } = input as SheetInputSelectField
+      return (
+        <Input
+          select
+          SelectProps={isMultiSelect ? { multiple: true } : undefined}
+        >
+          {options.map((option, optionIndex) => (
+            <MenuItem key={optionIndex} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Input>
+      )
+    }
+    case "checkbox": {
+      const { quantity, isPrecisionRating, numberValue } =
+        input as SheetInputCheckboxField
 
-    const Input: FunctionComponent<TextFieldProps> = ({
-        children,
-        ...rest
-    }) => {
+      const RenderRating = () => {
         const [value, setValue] = useState(templateValue)
-        const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-            setValue(event.currentTarget.value)
-            handleChangeSheetValues(blockIndex, inputIndex, event.currentTarget.value)
-        }
-        const defaultProps: TextFieldProps = {
-            label,
-            value,
-            type,
-            style: getGridArea(position),
-            variant: "standard",
-            fullWidth: true,
-            onChange: handleChange,
+        const handleChange = (_: any, ratingValue: any) => {
+          setValue(ratingValue)
+          handleChangeSheetValues(blockIndex, inputIndex, ratingValue)
         }
         return (
-            <TextField {...defaultProps} {...rest}>
-                {children}
-            </TextField>
+          <Rating
+            value={value as number}
+            max={quantity}
+            precision={isPrecisionRating ? 0.5 : 1}
+            defaultValue={0}
+            icon={<CheckBoxIcon />}
+            emptyIcon={<CheckBoxOutlineBlankIcon />}
+            onChange={handleChange}
+          />
         )
-    }
+      }
 
-    switch (type) {
-        case "number":
-        case "text":
-            return <Input />
-        case "select": {
-            const { options, isMultiSelect } = input
-            return (
-                <Input
-                    select
-                    SelectProps={
-                        isMultiSelect ? { multiple: true } : undefined
-                    }
-                >
-                    {options.map((option, optionIndex) => (
-                        <MenuItem key={optionIndex} value={option}>
-                            {option}
-                        </MenuItem>
-                    ))}
-                </Input>
-            )
+      const NumberValue = () => {
+        const [value, setValue] = useState(numberValue)
+        const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+          setValue(Number(event.currentTarget.value))
+          handleChangeSheetValues(
+            blockIndex,
+            inputIndex,
+            Number(event.currentTarget.value),
+            "numberValue"
+          )
         }
-        case "checkbox": {
-            const { quantity, isPrecisionRating, numberValue } = input
 
-            const RenderRating = () => {
-                const [value, setValue] = useState(templateValue)
-                const handleChange = (_: any, ratingValue: any) => {
-                    setValue(ratingValue)
-                    handleChangeSheetValues(blockIndex, inputIndex, ratingValue)
-                }
-                return <Rating
-                    value={value as number}
-                    max={quantity}
-                    precision={isPrecisionRating ? 0.5 : 1}
-                    defaultValue={0}
-                    icon={<CheckBoxIcon />}
-                    emptyIcon={<CheckBoxOutlineBlankIcon />}
-                    onChange={handleChange}
-                />
-            }
+        return (
+          <TextField
+            sx={{
+              width: "2.5rem",
+              input: { textAlign: "center", padding: ".25rem" },
+            }}
+            type="number"
+            inputProps={{
+              min: -999,
+              max: 999,
+              pattern: "d+",
+            }}
+            value={value}
+            onChange={handleChange}
+          />
+        )
+      }
 
-            const NumberValue = () => {
-                const [value, setValue] = useState(numberValue)
-                const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-                    setValue(Number(event.currentTarget.value))
-                    handleChangeSheetValues(blockIndex, inputIndex, Number(event.currentTarget.value), 'numberValue')
-                }
-
-                return (
-                    <TextField
-                        sx={{ width: '2.5rem', 'input': { textAlign: 'center', padding: '.25rem' } }}
-                        type="number"
-                        inputProps={{
-                            min: -999,
-                            max: 999,
-                            pattern: '\d+'
-                        }}
-                        value={value}
-                        onChange={handleChange}
-                    />
-                )
-            }
-
-            return (
-                <Box sx={{ display: "flex", marginTop: '1rem' }} >
-                    {quantity === 1 && <RenderRating />}
-                    <Typography flexGrow={1} component="legend">{label}</Typography>
-                    {quantity > 1 && <RenderRating />}
-                    {!isNil(numberValue) && <NumberValue />}
-
-                </Box>
-            )
-        }
+      return (
+        <Box sx={{ display: "flex", marginTop: "1rem" }}>
+          {quantity === 1 && <RenderRating />}
+          <Typography flexGrow={1} component="legend">
+            {label}
+          </Typography>
+          {quantity > 1 && <RenderRating />}
+          {!isNil(numberValue) && <NumberValue />}
+        </Box>
+      )
     }
+  }
 }
