d69cfc406be5363e8980b9b2863d42359c08c49c
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2022-05-08 16:47:52 -0300

    Feature/coming soon page (#2)
    * Add desktop coming soon page
    
    * Add mobile layout to coming soon page
    
    * Update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,38 @@
-import "normalize.css";
-import { FunctionComponent } from "react";
+import Head from "next/head"
+import { FunctionComponent } from "react"
 
 export default function MyApp({
   Component,
   pageProps,
 }: {
-  Component: FunctionComponent;
-  pageProps: Record<string, any>;
+  Component: FunctionComponent
+  pageProps: Record<string, any>
 }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Dice Overdrive</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="shortcut icon" href="/d10-electric.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <div className="page-layout">
+        <Component {...pageProps} />
+        <style jsx global>{`
+          body {
+            margin: 0;
+          }
+        `}</style>
+      </div>
+    </>
+  )
 }
