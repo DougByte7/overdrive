ddcaf9157face4ee6c5de40ce1ef9399c79118ae
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2022-05-08 16:26:58 -0300

    Add mobile layout to coming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,35 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main className="container">
-      <Image
-        src="/images/FULL-LOGO.svg"
-        alt="d10 Logo: Dice Overdrive. Charge your rolls"
-        width={731}
-        height={301}
-        layout="fixed"
-      />
+      <div className="logo-text">
+        <Image
+          src="/images/LOGO-TEXT.svg"
+          alt="Dice Overdrive. Charge your rolls"
+          width={414}
+          height={129}
+        />
+
+        <Image
+          aria-hidden={true}
+          src="/d10-electric.svg"
+          width={194}
+          height={222}
+        />
+      </div>
+
+      <div className="logo">
+        <Image
+          src="/images/FULL-LOGO.svg"
+          alt="d10 Logo: Dice Overdrive. Charge your rolls"
+          width={731}
+          height={301}
+          layout="fixed"
+        />
+      </div>
 
       <div className="banner">
         <p className="warning">
-          Website Under
+          Website&nbsp;Under
           <br />
           Construction
         </p>
@@ -25,10 +43,20 @@ export default function Home() {
           height: 100vh;
           width: 100%;
           display: grid;
-          grid-template-rows: 50% 1fr;
+          grid-template-rows: 60% 1fr;
           grid-template-columns: 100%;
           justify-items: center;
           align-items: center;
+        }
+
+        .logo-text {
+          display: flex;
+          flex-direction: column;
+          gap: 32px;
+        }
+
+        .logo {
+          display: none;
         }
 
         .banner {
@@ -50,11 +78,30 @@ export default function Home() {
           color: #121212;
           width: 100%;
           background: #ff6f00;
-          padding: 24px;
+          padding: 24px 0;
           text-align: center;
           text-transform: uppercase;
-          font-size: 3rem;
+          font-size: 2.5rem;
           font-family: "Inter", sans-serif;
+          margin: 0;
+        }
+
+        @media screen and (min-width: 750px) {
+          .container {
+            grid-template-rows: 50% 1fr;
+          }
+
+          .logo-text {
+            display: none;
+          }
+
+          .logo {
+            display: initial;
+          }
+
+          .warning: {
+            font-size: 3rem;
+          }
         }
       `}</style>
     </main>
