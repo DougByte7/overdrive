f6c7dfbb2bc3fe5b5f8a950b8c2b0261457aa21d
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2022-07-27 14:06:37 -0300

    Add custom scrollbar for chrome and remove number arrows

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,39 @@ export default function MyApp({
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <Component {...pageProps} />
+            <style jsx global>{`
+              body {
+                /* width */
+                ::-webkit-scrollbar {
+                  width: 5px;
+                }
+
+                /* Track */
+                ::-webkit-scrollbar-track {
+                  background: #f1f1f1;
+                }
+
+                /* Handle */
+                ::-webkit-scrollbar-thumb {
+                  background: #ff6f00;
+                }
+
+                /* Handle on hover */
+                ::-webkit-scrollbar-thumb:hover {
+                  background: rgb(178, 77, 0);
+                }
+
+                /* Remove Arrows/Spinners */
+                & input::-webkit-outer-spin-button,
+                input::-webkit-inner-spin-button,
+                input[type="number"] {
+                  /* Chrome, Safari, Edge, Opera */
+                  -webkit-appearance: none;
+                  /* Firefox */
+                  -moz-appearance: textfield;
+                }
+              }
+            `}</style>
           </ThemeProvider>
         </SharedSheetInfoProvider>
       </IndexedDBContextProvider>
