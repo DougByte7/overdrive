6773b051b3751053a3214b302f6f7d63c77f976a
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-02-05 00:55:06 -0300

    Add login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import dynamic from "next/dynamic"
 import theme from "@/theme"
 import { SharedSheetInfoProvider } from "@/shared-states/shared-sheet-info"
 import { ThemeProvider } from "@mui/material/styles"
-import CssBaseline from "@mui/material/CssBaseline"
 import { Analytics } from "@vercel/analytics/react"
+import { MantineProvider } from "@mantine/core"
 
 const IndexedDBContextProvider = dynamic(
   () => import("@/indexed-db/indexed-db-context"),
@@ -29,9 +29,30 @@ export default function MyApp({
       <IndexedDBContextProvider>
         <SharedSheetInfoProvider>
           <ThemeProvider theme={theme}>
-            <CssBaseline />
-
-            <Component {...pageProps} />
+            <MantineProvider
+              theme={{
+                fontFamily: "Manrope",
+                primaryColor: "brand",
+                colors: {
+                  brand: [
+                    "#b366d8",
+                    "#a74dd2",
+                    "#9a33cb",
+                    "#8e1ac5",
+                    "#8100be",
+                    "#7400ab",
+                    "#670098",
+                    "#5a0085",
+                    "#4d0072",
+                    "#41005f",
+                  ],
+                },
+              }}
+              withGlobalStyles
+              withNormalizeCSS
+            >
+              <Component {...pageProps} />
+            </MantineProvider>
           </ThemeProvider>
         </SharedSheetInfoProvider>
       </IndexedDBContextProvider>
