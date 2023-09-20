4542b62d2251936b7cc1db04529e19b4cc781612
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-09-19 22:10:56 -0300

    Feat/spell filter (#24)
    * Fix no spells size
    
    * Add dnd monsters
    
    * Add monster list page
    
    * Fix class type
    
    * Fix lv4 feature name
    
    * Update mantine
    
    * Fix some class lvup issues
    
    * Change spells to be array instead of set
    
    * Fix whe to show spell selection step
    
    * Add level up system
    
    * fix build error
    
    * Fix spell sort
    
    * Fix spell card style
    
    * Fix duplicated spells
    
    * Add spell filters
    
    * Change grimoire to sort spells by name
    
    * Fix build

--- src/components/character/state.ts
+++ src/components/character/state.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai"
-import { Spell } from "./components/grimoire"
-import { CharacterForm } from "../home/character-builder/interfaces"
+import type { CharacterForm } from "../home/character-builder/interfaces"
+import type { DnD5eSpell } from "@/assets/dnd/5e/interfaces"
 
 export const characterAtom = atom<CharacterForm | null>(null)
 
@@ -8,5 +8,4 @@ export const activeTabAtom = atom<"basic" | "inventory" | "skills" | "magic">(
   "basic"
 )
 
-export const selectedSpellAton = atom<Spell | null>(null)
-
+export const selectedSpellAton = atom<DnD5eSpell | null>(null)
