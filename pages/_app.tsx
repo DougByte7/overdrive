72ce130acfd11108f4011d650f7e7fa93848ecb1
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-09-20 18:58:12 -0300

    Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,6 +35,19 @@ export default function MyApp({
   return (
     <>
       <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://diceoverdrive.com" />
+        <meta property="og:site_name" content="Dice Overdrive" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:locale:alternate" content="en_US" />
+        <meta
+          property="og:description"
+          content="Dice Overdrive, sua plataforma definitiva para jogar RPG. Pegue os dados e vamos jogar!"
+        />
+        <meta
+          name="keywords"
+          content="dice, overdrive, vtt, virtual, tabletop, jogar, rpg, online"
+        />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
