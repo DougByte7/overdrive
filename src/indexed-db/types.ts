85c0340d80a5aec95a6d87985b759280ec8787e0
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-10-02 20:30:36 -0300

    Remove indexedDB types

--- src/indexed-db/types.ts
+++
@@ -1,8 +0,0 @@
-export interface IOverdriveDBData {
-  addItems: (storeNames: Array<OverdriveDBStores>, data: any[]) => Promise<void>
-  getItems: (storeName: OverdriveDBStores) => Promise<any>
-}
-
-export enum OverdriveDBStores {
-  DICTIONARIES = "dictionaries",
-}
