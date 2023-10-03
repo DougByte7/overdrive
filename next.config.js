ef15319b3ae86b006d040ef7907c67826d0deed6
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-10-03 18:41:39 -0300

    update dps

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require("@sentry/nextjs")
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-})
+});
 
 module.exports = withSentryConfig(
   withPWA({
+    experimental: { appDir: true },
     i18n: {
       locales: ["en-US", "pt-BR"],
       defaultLocale: "pt-BR",
@@ -18,5 +19,5 @@ module.exports = withSentryConfig(
     reactStrictMode: true,
   }),
   { silent: true },
-  { hideSourcemaps: true }
-)
+  { hideSourcemaps: true },
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
+    colors: {
+      brand: {
+        700: "#670098",
+        500: "#8100be",
+        200: "#f6f0fa",
+      },
+    },
   },
   plugins: [],
-}
+};
