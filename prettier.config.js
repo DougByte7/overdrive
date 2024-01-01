91b7668ecaa1a5ef45b76757a7d827485a78177d
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2024-01-01 20:19:03 -0300

    Add import sorter

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,14 @@
 // prettier.config.js
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-}
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+  importOrder: ["^@/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
