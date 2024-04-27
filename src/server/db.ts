388c9d53724ed442c09fdf7ab7d58e4fcdcc58ee
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2024-04-27 12:59:19 -0300

    Change to Neon db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,14 +1,23 @@
-import { PrismaClient } from "@prisma/client";
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+import ws from 'ws'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+dotenv.config()
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        adapter: new PrismaNeon(new Pool({ connectionString })),
+        log:
+            process.env.NODE_ENV === 'development'
+                ? ['query', 'error', 'warn']
+                : ['error'],
+    })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
