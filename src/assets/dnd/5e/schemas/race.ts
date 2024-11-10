7c0a3f0cd5089acc9e17775af6cb02e2556e3043
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2024-11-10 20:45:38 -0300

    Fix valibot

--- src/assets/dnd/5e/schemas/race.ts
+++ src/assets/dnd/5e/schemas/race.ts
@@ -5,33 +5,55 @@ import {
     minLength,
     number,
     object,
+    pipe,
     string,
 } from 'valibot'
 
 export const CustomRaceSchema = object({
     public: boolean(),
-    name: string([minLength(1, 'O campo deve ter pelo menos um caractere')]),
+    name: pipe(
+        string(),
+        minLength(1, 'O campo deve ter pelo menos um caractere')
+    ),
     description: string(),
     speed: object({
-        land: number([integer('A velocidade precisa ser um número inteiro')]),
-        climb: number([integer('A velocidade precisa ser um número inteiro')]),
-        fly: number([integer('A velocidade precisa ser um número inteiro')]),
-        swimming: number([
-            integer('A velocidade precisa ser um número inteiro'),
-        ]),
-        burrow: number([integer('A velocidade precisa ser um número inteiro')]),
+        land: pipe(
+            number(),
+            integer('A velocidade precisa ser um número inteiro')
+        ),
+        climb: pipe(
+            number(),
+            integer('A velocidade precisa ser um número inteiro')
+        ),
+        fly: pipe(
+            number(),
+            integer('A velocidade precisa ser um número inteiro')
+        ),
+        swimming: pipe(
+            number(),
+            integer('A velocidade precisa ser um número inteiro')
+        ),
+        burrow: pipe(
+            number(),
+            integer('A velocidade precisa ser um número inteiro')
+        ),
     }),
     type: string(),
     size: string(),
-    darkvision: number([integer('O campo precisa ser um número inteiro')]),
+    darkvision: pipe(
+        number(),
+        integer('O campo precisa ser um número inteiro')
+    ),
     traits: array(
         object({
-            name: string([
-                minLength(1, 'O campo deve ter pelo menos um caractere'),
-            ]),
-            description: string([
-                minLength(1, 'O campo deve ter pelo menos um caractere'),
-            ]),
+            name: pipe(
+                string(),
+                minLength(1, 'O campo deve ter pelo menos um caractere')
+            ),
+            description: pipe(
+                string(),
+                minLength(1, 'O campo deve ter pelo menos um caractere')
+            ),
         })
     ),
 })
